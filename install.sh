2dddc07314b457b9bb1372037ac950c49cfdb344
srazmetov <razmser@gmail.com>
2015-10-10 23:55:44 +0300

    tmux configs moved to /home/srazmetov folder

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -rf ~/backup
 mkdir ~/backup
 mv ~/.zshrc ~/backup
 mv ~/.vimrc ~/backup
-mv /etc/tmux.conf ~/backup
+mv ~/.tmux.conf ~/backup
 mv ~/.config/Terminal/terminalrc ~/backup
 mv ~/.vim/colors/solarized.vim ~/backup
 mkdir -p ~/.config/Terminal/
@@ -12,7 +12,7 @@ mkdir -p ~/.vim/colors/
 
 ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/tmux.conf /etc/tmux.conf
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/solarized/xfce4-terminal/dark/terminalrc ~/.config/Terminal/terminalrc
 ln -s $(pwd)/solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 
