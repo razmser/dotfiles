6c7a643ac78331f0f9115e994e632e42dc095c64
srazmetov <razmser@gmail.com>
2015-10-11 02:38:35 +0300

    Add rtorrent config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ mkdir ~/backup
 mv ~/.zshrc ~/backup
 mv ~/.vimrc ~/backup
 mv ~/.tmux.conf ~/backup
+mv ~/.rtorrent.rc ~/backup
 mv ~/.config/Terminal/terminalrc ~/backup
 mv ~/.vim/colors/solarized.vim ~/backup
 mkdir -p ~/.config/Terminal/
@@ -13,6 +14,7 @@ mkdir -p ~/.vim/colors/
 ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/rtorrent.rc ~/.rtorrent.rc
 ln -s $(pwd)/solarized/xfce4-terminal/dark/terminalrc ~/.config/Terminal/terminalrc
 ln -s $(pwd)/solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 
