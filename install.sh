e50232b627c25258fca90468cba9e53d5b40b8b3
srazmetov <razmser@gmail.com>
2015-10-10 21:25:23 +0300

    Use solarized dark color scheme in xfce4-terminal

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ mkdir ~/backup
 mv ~/.zshrc ~/backup
 mv ~/.vimrc ~/backup
 mv /etc/tmux.conf ~/backup
+mkdir -p ~/.config/Terminal/
 
 ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/tmux.conf /etc/tmux.conf
+ln -s $(pwd)/solarized/xfce4-terminal/dark/terminalrc ~/.config/Terminal/terminalrc
 
