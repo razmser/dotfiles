ec0b7a2325343c4588b99cc8c65f926bf439f6c1
Sergei Razmetov <razmser@gmail.com>
2023-08-04 01:35:35 +0300

    add git abbreviations to fish

---
+++ fish/conf.d/git.fish
@@ -0,0 +1,9 @@
+abbr --add gs git status
+abbr --add gco git checkout
+abbr --add gu git pull
+abbr --add gp git push origin HEAD
+abbr --add gr git rebase
+abbr --add ga git add
+abbr --add grm git rm
+abbr --add gd git diff
+abbr --add gc git commit
