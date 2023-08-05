cc79b6124456f091b59307aba0c45af259d2fc15
Sergei Razmetov <razmser@gmail.com>
2023-08-05 19:05:33 +0300

    [nvim] add which-key and configure telescope remaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,18 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local telescope = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>?', telescope.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', telescope.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuraion to telescope to change theme, layout, etc.
+  telescope.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+vim.keymap.set('n', '<leader>gf', telescope.git_files, { desc = "Search [G]it [F]iles" })
+vim.keymap.set('n', '<leader>sf', telescope.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set('n', '<leader>sh', telescope.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set('n', '<leader>sg', telescope.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set('n', '<leader>sd', telescope.diagnostics, { desc = "[S]earch [D]iagnostics" })
 
