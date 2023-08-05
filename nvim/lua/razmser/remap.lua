785b0fa24611a92c424b51940c6e9e62eeb0cc3f
Sergei Razmetov <razmser@gmail.com>
2023-08-05 18:46:54 +0300

    [nvim] few remaps

--- nvim/lua/razmser/remap.lua
+++ nvim/lua/razmser/remap.lua
@@ -1,3 +1,11 @@
 -- open Netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move visual selection up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gV=GV")
+
+-- use copy into system clipboard with different command
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
