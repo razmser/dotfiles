f64d3da3abafa081a237e1bbd74fe8445d539e8b
Sergei Razmetov <razmser@gmail.com>
2024-03-18 11:53:02 +0300

    [wezterm] Add basic wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.color_scheme = 'Catppuccin Frappe'
+
+-- https://wezfurlong.org/wezterm/config/lua/wezterm/target_triple.html
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
+-- Mac OS has Super + C/P for Copy/Paste so we don't need this hacks
+if not is_darwin() then
+  config.keys = {
+    -- Ctrl + C works as copy when something is selected, otherwise it's interrupt
+    -- based on https://github.com/wez/wezterm/discussions/2426#discussioncomment-4197449
+    {
+      key = 'c',
+      mods = 'CTRL',
+      action = wezterm.action_callback(function(window, pane)
+        local sel = window:get_selection_text_for_pane(pane)
+        if (not sel or sel == '') then
+          window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+        else
+          window:perform_action(wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' }, pane)
+        end
+      end),
+    },
+    { key = 'v', mods = 'CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
+    { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+      window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+    },
+    { key = 'V', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+      window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+    },
+  }
+end
+
+return config
+
