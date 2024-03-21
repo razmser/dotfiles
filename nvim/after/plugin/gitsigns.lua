a777568b1c89a0271aa51fbcc9a209d49f7ad6c0
Sergei Razmetov <razmser@gmail.com>
2024-03-21 12:35:37 +0300

    [nvim] Add git hunk navigation

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,4 +1,29 @@
 require('gitsigns').setup {
   numhl              = true,
   current_line_blame = true,
+
+  -- based on doc example
+  -- https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#keymaps
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+  end
 }
