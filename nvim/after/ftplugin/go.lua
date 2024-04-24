811c6145ac23047ab3760d7253fedd69a8bf2c08
Sergei Razmetov <razmser@gmail.com>
2024-04-24 15:36:16 +0300

    [nvim] Disable vim-sleuth and editorconfig for go
    nvim has builtin support for .editorconfig so I don't need sleuth
    
    for go I don't want to have tabs more then 4 spaces,
        even if it's in .editorconfig and I have go fmt anyway

---
+++ nvim/after/ftplugin/go.lua
@@ -0,0 +1,6 @@
+-- I have indent_size = 8 in go project that I mostly work on
+-- Since I hate it I just disabled editorconfig support for go altogether
+-- kinda assume that go fmt will fix stuff anyway
+vim.b.editorconfig = false
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
