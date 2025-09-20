8e56f87de6e91e58aa0352eeec3def3bb9ef825a
Sergei Razmetov <razmser@gmail.com>
2025-09-20 23:02:09 +0300

    fix(nvim): plugin/lint.lua config syntax

--- nvim/after/plugin/lint.lua
+++ nvim/after/plugin/lint.lua
@@ -2,7 +2,7 @@ require('lint').linters_by_ft = {
   yaml = {'yamllint'},
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" "BufRead" }, {
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufRead" }, {
   callback = function()
     require("lint").try_lint()
   end,
